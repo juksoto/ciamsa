c795c558e720ec05673ef2295d918b7db6f37f40
Juan Soto <juksoto@gmail.com>
2016-05-16 10:01:59 -0500

    Inicio del proyecto
    Diseño listo. Empezamos el desarrollo

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>CIAMSA - Conozca el producto para nutrir su cultivo</title>
+   	<link rel="stylesheet" href="assets/css/foundation.css" />
+    <link href="assets/css/normalize.css" rel="stylesheet">
+    <link href="vendors/bootflat/css/bootflat.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<!-- Step One -->
+	<section class="wrapper-step">
+		<!-- NAV -->
+		<nav>
+			<section class="row">
+	  			<section class="small-12 column text-right">
+	  				<a href="" class="button btn-ciamsa"> <span class="icon-user" aria-hidden="true"></span> Solicitar cotización</a>
+	  			</section>
+			</section>
+		</nav>
+		<!-- end NAV -->
+		<!-- content --> 
+		<section class="content">
+			<!-- Logos -->
+			<section class="row">
+				<section class="small-10 small-centered column text-center">
+					<ul class="row small-up-2 medium-up-4 list-logo" >
+					  <li class="column text-center">
+						  	<img src="images/logo_forkamix.png" alt="Forkamix">
+					  </li>
+					  <li class="column text-center">
+						  	<img src="images/logo_nutrikimia.png" alt="Nutrikimia">
+					  </li>
+					  <li class="column text-center">
+						  	<img src="images/logo_nitroeffi.png" alt="Nitro Effi100">
+					  </li>
+					  <li class="column text-center">
+						  	<img src="images/logo_solucion_uan.png" alt="Solucion UAN">
+					  </li>
+					</ul>
+				</section>
+			</section>	
+			<!-- End  Logos -->
+			<!-- header image -->
+			<section class="row header-image">
+				<section class="small-12 column text-center">
+					<a href="">
+						<img src="images/producto_nutrir_su_cultivo.png" alt="Conozca el producto para nutrir su cultivo" width="490">
+					</a>
+					<h1>Conozca 
+						<span class="color-ciamsa2"> 
+							el producto 
+						</span> 
+						<span class="small">
+							para nutrir su cultivo
+						</span>
+					</h1>
+					<section class="row">
+						<article class="small-5 medium-3 column text-center small-centered medium-centered">
+							<a href="" class="button btn-grapefruit expanded">
+								Iniciar aquí
+						 	</a>	
+						</article>
+					</section>
+
+				</section>
+			</section>
+			<!-- End  header image -->
+		</section>	
+		<!-- End  content -->
+		<!-- Bottom -->
+		<section class="row bottom">
+			<section class="small-12 column text-center">
+					<img src="images/bottom-ciamsa.png" alt="Expertos en mezclas" width="694">
+			</section>
+		</section>	
+		<!-- End  Bottom -->
+	</section>
+	<!-- End Step One -->
+	<footer class="row expanded">
+		<section class="small-12 text-center">
+			<p>
+				Desarrollo por Innova Corporación S.A.S
+			</p>
+		</section>
+	</footer>
+    <script src="assets/js/vendor/jquery.js"></script>
+    <script src="assets/js/vendor/what-input.js"></script>
+    <script src="assets/js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+</body>
+</html>
---
+++ step-one.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>CIAMSA - Selecciona el cultivo</title>
+   	<link rel="stylesheet" href="assets/css/foundation.css" />
+    <link href="assets/css/normalize.css" rel="stylesheet">
+    <link href="vendors/bootflat/css/bootflat.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<!-- Step One -->
+	<section class="wrapper-step-one">
+		<!-- NAV -->
+		<nav>
+			<section class="row">
+	  			<section class="small-12 column text-right">
+	  				<a href="" class="button btn-sunflower"> <span class="icon-home" aria-hidden="true"></span> Inicio</a>
+	  				<a href="" class="button btn-aqua"> <span class="icon-back" aria-hidden="true" style="padding-top:5px"></span> Regresar</a>
+	  				<a href="" class="button btn-ciamsa"> <span class="icon-user" aria-hidden="true"></span> Solicitar cotización</a>
+	  			</section>
+			</section>
+		</nav>
+		<!-- end NAV -->
+		<!-- content --> 
+		<section class="content">
+			<!-- title -->
+			<section class="row">
+				<section class="small-10 small-centered column text-center">
+					<article>
+						<h2>
+							<span class="step">
+								Paso 1 de 3
+							</span>
+							Tipos de Cultivos
+							<span class="textline">
+								Seleccione su tipo de cultivo
+							</span>
+						</h2>
+					</article>
+				</section>
+			</section>	
+			<!-- End  title -->
+			<!-- content -->
+			<section class="row content">
+				<section class="small-12 column text-center">
+					
+				</section>
+			</section>
+			<!-- End  content -->
+		</section>	
+		<!-- End  content -->
+		<!-- Bottom -->
+		<section class="row bottom">
+			<section class="small-12 column text-center">
+					<img src="images/bottom-ciamsa.png" alt="Expertos en mezclas" width="694">
+			</section>
+		</section>	
+		<!-- End  Bottom -->
+	</section>
+	<!-- End Step One -->
+	<footer class="row expanded">
+		<section class="small-12 text-center">
+			<p>
+				Desarrollo por Innova Corporación S.A.S
+			</p>
+		</section>
+	</footer>
+    <script src="assets/js/vendor/jquery.js"></script>
+    <script src="assets/js/vendor/what-input.js"></script>
+    <script src="assets/js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+</body>
+</html>
